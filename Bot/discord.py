482b8d5f16cc162c19b306c469abb2f971024a91
Deutscher <81298350+Deutscher775@users.noreply.github.com>
2023-07-29 18:12:49 +0200

    Add files via upload

--- Bot/discord.py
+++ Bot/discord.py
@@ -11,13 +11,13 @@ client = commands.Bot(command_prefix="gc!", intents=nextcord.Intents.all())
 @client.event
 async def on_message_delete(message: nextcord.Message):
     if message.channel.id == \
-            requests.get(f"http://api.guildcord.deutscher775.de/{message.guild.id}?token={config.MASTER_TOKEN}").json()[
+            requests.get(f"https://api.guildcord.deutscher775.de/{message.guild.id}?token={config.MASTER_TOKEN}").json()[
                 "config"]["channels"]["discord"]:
         embed = nextcord.Embed(title=f"{message.author.name} - Deleted", description=message.content, colour=0xf5c400)
         embed.add_field(name="Created at",
                         value=f'{datetime.datetime.strftime(message.created_at, "%Y-%m-%d %H:%M:%S")} UTC')
         await client.get_channel(
-            requests.get(f"http://api.guildcord.deutscher775.de/{message.guild.id}?token={config.MASTER_TOKEN}").json()[
+            requests.get(f"https://api.guildcord.deutscher775.de/{message.guild.id}?token={config.MASTER_TOKEN}").json()[
                 "config"]["logs"][
                 "discord"]).send(embed=embed)
 
@@ -25,7 +25,7 @@ async def on_message_delete(message: nextcord.Message):
 @client.event
 async def on_message_edit(before: nextcord.Message, after: nextcord.Message):
     if before.channel.id == \
-            requests.get(f"http://api.guildcord.deutscher775.de/{before.guild.id}?token={config.MASTER_TOKEN}").json()[
+            requests.get(f"https://api.guildcord.deutscher775.de/{before.guild.id}?token={config.MASTER_TOKEN}").json()[
                 "config"]["channels"]["discord"]:
         embed = nextcord.Embed(title=f"{before.author.name} - Edited", description=after.content, colour=0xf5c400)
         embed.add_field(name="Jump", value=after.jump_url)
@@ -33,7 +33,7 @@ async def on_message_edit(before: nextcord.Message, after: nextcord.Message):
         embed.add_field(name="After", value=after.content, inline=False)
 
         await client.get_channel(
-            requests.get(f"http://api.guildcord.deutscher775.de/{before.guild.id}?token={config.MASTER_TOKEN}").json()[
+            requests.get(f"https://api.guildcord.deutscher775.de/{before.guild.id}?token={config.MASTER_TOKEN}").json()[
                 "config"]["logs"][
                 "discord"]).send(embed=embed)
 
@@ -42,11 +42,11 @@ async def on_message_edit(before: nextcord.Message, after: nextcord.Message):
 async def on_message(message: nextcord.Message):
     try:
         if message.channel.id == requests.get(
-                f"http://api.guildcord.deutscher775.de/{message.guild.id}?token={config.MASTER_TOKEN}").json()[
+                f"https://api.guildcord.deutscher775.de/{message.guild.id}?token={config.MASTER_TOKEN}").json()[
             "config"]["channels"][
             "discord"]:
             blacklist = \
-            requests.get(f"http://api.guildcord.deutscher775.de/{message.guild.id}?token={config.MASTER_TOKEN}").json()[
+            requests.get(f"https://api.guildcord.deutscher775.de/{message.guild.id}?token={config.MASTER_TOKEN}").json()[
                 "config"]["blacklist"]
             for word in blacklist:
                 if word is not None:
@@ -54,22 +54,23 @@ async def on_message(message: nextcord.Message):
                         embed = nextcord.Embed(title=f"{message.author.name} - Flagged", description=message.content,
                                                colour=0xf5c400)
                         await client.get_channel(requests.get(
-                            f"http://api.guildcord.deutscher775.de/{message.guild.id}?token={config.MASTER_TOKEN}").json()[
+                            f"https://api.guildcord.deutscher775.de/{message.guild.id}?token={config.MASTER_TOKEN}").json()[
                                                      "config"]["logs"][
                                                      "discord"]).send(embed=embed)
                         return
                     else:
                         pass
             else:
-                if not message.author.bot:
+                allowed_ids = requests.get(f"https://api.guildcord.deutscher775.de/{message.guild.id}?token={config.MASTER_TOKEN}").json()["config"]["allowed-ids"]
+                if not message.author.bot or message.author.id in allowed_ids:
                     if not message.attachments:
                         requests.post(
-                            f"http://api.guildcord.deutscher775.de/update/{message.channel.guild.id}?message_content={message.content}&"
+                            f"https://api.guildcord.deutscher775.de/update/{message.channel.guild.id}?message_content={message.content}&"
                             f"message_author_name={message.author.name}&message_author_avatar={message.author.avatar.url}&"
                             f"message_author_id={message.author.id}&trigger=true&sender=discord&token={config.MASTER_TOKEN}")
                     if message.attachments:
                         requests.post(
-                            f"http://api.guildcord.deutscher775.de/update/{message.channel.guild.id}?message_content={message.content}&"
+                            f"https://api.guildcord.deutscher775.de/update/{message.channel.guild.id}?message_content={message.content}&"
                             f"message_author_name={message.author.name}&message_author_avatar={message.author.avatar.url}&"
                             f"message_author_id={message.author.id}&trigger=true&sender=discord&token={config.MASTER_TOKEN}")
 
@@ -80,14 +81,14 @@ async def on_message(message: nextcord.Message):
 @client.slash_command(name="register", default_member_permissions=8, description="Automatically register your server.")
 async def register(interaction: nextcord.Interaction):
     webhook = await interaction.channel.create_webhook(name="Guildcord")
-    requests.post(f"http://api.guildcord.deutscher775.de/create?endpoint={interaction.guild.id}")
+    requests.post(f"https://api.guildcord.deutscher775.de/create?endpoint={interaction.guild.id}")
     requests.post(
-        f"http://api.guildcord.deutscher775.de/update/{interaction.channel.guild.id}?channel_discord={interaction.channel.id}&"
+        f"https://api.guildcord.deutscher775.de/update/{interaction.channel.guild.id}?channel_discord={interaction.channel.id}&"
         f"webhook_discord={webhook.url}&token={config.MASTER_TOKEN}")
     token = requests.post(
-        f"http://api.guildcord.deutscher775.de/token/{interaction.guild.id}?master_token={config.MASTER_TOKEN}")
+        f"https://api.guildcord.deutscher775.de/token/{interaction.guild.id}?master_token={config.MASTER_TOKEN}")
     await interaction.response.send_message(
-        f"Created enpoint: http://api.guildcord.deutscher775.de/{interaction.guild.id}\n"
+        f"Created enpoint: https://api.guildcord.deutscher775.de/{interaction.guild.id}\n"
         f"Your API Token is: `{token.json()['token']}`\n"
         f"Save this and **__do not__** share this!\n"
         f"Hop over to Guilded and run this command in the channel, you want do bridge over:"
@@ -96,31 +97,96 @@ async def register(interaction: nextcord.Interaction):
 
 @client.slash_command(name="help")
 async def help(interaction: nextcord.Interaction):
-    embed = nextcord.Embed(title="Guildcord", description="API Docs: http://api.guildcord.deutscher775.de")
+    embed = nextcord.Embed(title="Guildcord", description="API Docs: https://api.guildcord.deutscher775.de")
     embed.add_field(name="register", value="Register you server.", inline=False)
     embed.add_field(name="delete", value="Delete your endpoint.", inline=False)
     embed.add_field(name="generate-token", value="Generate a new token for your endpoint.", inline=False)
+    embed.add_field(name="allow", value="Allow a bot or webhook to be forwarded.", inline=False)
+    embed.add_field(name="set-log", value="Setup your logchannel.", inline=False)
 
     await interaction.response.send_message(embed=embed, ephemeral=True)
 
 
 @client.slash_command(name="delete", default_member_permissions=8, description="Delete your endpoint.")
 async def delete(interaction: nextcord.Interaction):
-    requests.delete(f"http://api.guildcord.deutscher775.de/delete/{interaction.guild.id}?token={config.MASTER_TOKEN}")
+    requests.delete(f"https://api.guildcord.deutscher775.de/delete/{interaction.guild.id}?token={config.MASTER_TOKEN}")
     await interaction.response.send_message(f"Deleted your endpoint.", ephemeral=True)
 
 
 @client.slash_command(name="set-log", default_member_permissions=8, description="Set logs")
 async def setlog(interaction: nextcord.Interaction, channel: nextcord.TextChannel):
-    requests.post(f"http://api.guildcord.deutscher775.de/update/{interaction.guild.id}?token={config.MASTER_TOKEN}"
+    requests.post(f"https://api.guildcord.deutscher775.de/update/{interaction.guild.id}?token={config.MASTER_TOKEN}"
                   f"&log_discord={channel.id}")
     await interaction.response.send_message(f"Updated your endpoint.", ephemeral=True)
+
+
+@client.slash_command(name="allow", default_member_permissions=8, description="Allow a webhook or bot")
+async def allow(interaction: nextcord.Interaction, id):
+    requests.post(f"https://api.guildcord.deutscher775.de/update/{interaction.guild.id}?token={config.MASTER_TOKEN}"
+                  f"&allowed_ids={id}")
+    await interaction.response.send_message(f"Updated your endpoint.", ephemeral=True)
+
+@client.slash_command(name="update", default_member_permissions=8, description="Update your endpoint.")
+async def update(interaction: nextcord.Interaction, action=nextcord.SlashOption(
+    name="action",
+    choices={
+        "webhook_discord", "webhook_guilded", "log_discord", "log_guilded", "channel_discord", "channel_guilded"}),
+                 *, value):
+    if action == "webhook_discord":
+        r = requests.post(f"https://api.guildcord.deutscher775.de/update/{interaction.guild.id}?token={config.MASTER_TOKEN}"
+                      f"&webhook_discord={value}")
+        if r.status_code == 200:
+            await interaction.response.send_message(f"Updated your endpoint.", ephemeral=True)
+        else:
+            await interaction.response.send_message(":x: - Something went wrong while posting to your endpoint.\n"
+                                                    f"Status code: `{r.status_code}`")
+    if action == "webhook_guilded":
+        r = requests.post(f"https://api.guildcord.deutscher775.de/update/{interaction.guild.id}?token={config.MASTER_TOKEN}"
+                      f"&webhook_guilded={value}")
+        if r.status_code == 200:
+            await interaction.response.send_message(f"Updated your endpoint.", ephemeral=True)
+        else:
+            await interaction.response.send_message(":x: - Something went wrong while posting to your endpoint.\n"
+                                                    f"Status code: `{r.status_code}`")
+    if action == "log_discord":
+        r = requests.post(f"https://api.guildcord.deutscher775.de/update/{interaction.guild.id}?token={config.MASTER_TOKEN}"
+                      f"&log_discord={value}")
+        if r.status_code == 200:
+            await interaction.response.send_message(f"Updated your endpoint.", ephemeral=True)
+        else:
+            await interaction.response.send_message(":x: - Something went wrong while posting to your endpoint.\n"
+                                                    f"Status code: `{r.status_code}`")
+    if action == "log_guilded":
+        r = requests.post(f"https://api.guildcord.deutscher775.de/update/{interaction.guild.id}?token={config.MASTER_TOKEN}"
+                      f"&log_guilded={value}")
+        if r.status_code == 200:
+            await interaction.response.send_message(f"Updated your endpoint.", ephemeral=True)
+        else:
+            await interaction.response.send_message(":x: - Something went wrong while posting to your endpoint.\n"
+                                                    f"Status code: `{r.status_code}`")
+    if action == "channel_discord":
+        r = requests.post(f"https://api.guildcord.deutscher775.de/update/{interaction.guild.id}?token={config.MASTER_TOKEN}"
+                      f"&channel_discord={value}")
+        if r.status_code == 200:
+            await interaction.response.send_message(f"Updated your endpoint.", ephemeral=True)
+        else:
+            await interaction.response.send_message(":x: - Something went wrong while posting to your endpoint.\n"
+                                                    f"Status code: `{r.status_code}`")
+    if action == "channel_guilded":
+        r = requests.post(f"https://api.guildcord.deutscher775.de/update/{interaction.guild.id}?token={config.MASTER_TOKEN}"
+                      f"&channel_guilded={value}")
+        if r.status_code == 200:
+            await interaction.response.send_message(f"Updated your endpoint.", ephemeral=True)
+        else:
+            await interaction.response.send_message(":x: - Something went wrong while posting to your endpoint.\n"
+                                                    f"Status code: `{r.status_code}`")
+
 
 
 @client.slash_command(name="generate-token", default_member_permissions=8, description="Generate a new API Token.")
 async def gen(interaction: nextcord.Interaction):
     token = requests.post(
-        f"http://api.guildcord.deutscher775.de/token/{interaction.guild.id}?master_token={config.MASTER_TOKEN}")
+        f"https://api.guildcord.deutscher775.de/token/{interaction.guild.id}?master_token={config.MASTER_TOKEN}")
     await interaction.response.send_message(f"Your new API Token is: `{token.json()['token']}`\n"
                                             f"Save this and **__do not__** share this!", ephemeral=True)
 
--- Bot/guilded_bot.py
+++ Bot/guilded_bot.py
@@ -17,13 +17,13 @@ async def on_message_delete(message: guilded.Message):
     endpoint_file = open(f"{pathlib.Path(__file__).parent.resolve()}/guilded_servers/{message.server.id}.json", "r")
     endpoint = json.load(endpoint_file)["discord"]
     if message.channel.id == \
-            requests.get(f"http://api.guildcord.deutscher775.de/{endpoint}?token={config.MASTER_TOKEN}").json()[
+            requests.get(f"https://api.guildcord.deutscher775.de/{endpoint}?token={config.MASTER_TOKEN}").json()[
                 "config"]["channels"]["guilded"]:
         embed = guilded.Embed(title=f"{message.author.name} - Deleted", description=message.content, colour=0xf5c400)
         embed.add_field(name="Created at",
                         value=f'{datetime.datetime.strftime(message.created_at, "%Y-%m-%d %H:%M.%S")} UTC')
         channel = await client.fetch_channel(
-            requests.get(f"http://api.guildcord.deutscher775.de/{endpoint}?token={config.MASTER_TOKEN}").json()[
+            requests.get(f"https://api.guildcord.deutscher775.de/{endpoint}?token={config.MASTER_TOKEN}").json()[
                 "config"]["logs"][
                 "guilded"])
         await channel.send(embed=embed)
@@ -35,7 +35,7 @@ async def on_message_edit(before: guilded.Message, after: guilded.Message):
     endpoint_file = open(f"{pathlib.Path(__file__).parent.resolve()}/guilded_servers/{before.server.id}.json", "r")
     endpoint = json.load(endpoint_file)["discord"]
     if before.channel.id == \
-            requests.get(f"http://api.guildcord.deutscher775.de/{endpoint}?token={config.MASTER_TOKEN}").json()[
+            requests.get(f"https://api.guildcord.deutscher775.de/{endpoint}?token={config.MASTER_TOKEN}").json()[
                 "config"]["channels"]["guilded"]:
         embed = guilded.Embed(title=f"{before.author.name} - Edited", description=after.content, colour=0xf5c400)
         embed.add_field(name="Jump", value=f"[Jump]({after.jump_url})")
@@ -43,7 +43,7 @@ async def on_message_edit(before: guilded.Message, after: guilded.Message):
         embed.add_field(name="After", value=after.content, inline=False)
 
         channel = await client.fetch_channel(
-            requests.get(f"http://api.guildcord.deutscher775.de/{endpoint}?token={config.MASTER_TOKEN}").json()[
+            requests.get(f"https://api.guildcord.deutscher775.de/{endpoint}?token={config.MASTER_TOKEN}").json()[
                 "config"]["logs"][
                 "guilded"])
         await channel.send(embed=embed)
@@ -63,51 +63,87 @@ async def on_message(message: guilded.Message):
             if endpoint == "":
                 await message.channel.send("Invalid Format: `gc!register DISCORD_SERVER_ID`")
             else:
+                for server in os.listdir(f"{pathlib.Path(__file__).parent.resolve()}/guilded_servers"):
+                    if server.endswith(".json"):
+                        try:
+                            discord_server = json.load(open(server, "r"))["discord"]
+                            if discord_server == endpoint:
+                                await message.channel.send(
+                                    f"Enpoint exists already: https://api.guildcord.deutscher775.de/{endpoint}")
+                                return
+                        except:
+                            pass
+                    else:
+                        pass
+
                 try:
                     data = {"discord": endpoint}
                     json.dump(data,
                               open(f"{pathlib.Path(__file__).parent.resolve()}/guilded_servers/{message.guild.id}.json",
                                    "x"))
                 except FileExistsError:
-                    await message.channel.send(f"Enpoint exists already: http://api.guildcord.deutscher775.de/{endpoint}")
+                    await message.channel.send(f"Enpoint exists already: https://api.guildcord.deutscher775.de/{endpoint}")
                     return
                 webhook = await message.channel.create_webhook(name="Guildcord")
                 requests.post(
-                    f"http://api.guildcord.deutscher775.de/update/{endpoint}?channel_guilded={message.channel.id}&"
+                    f"https://api.guildcord.deutscher775.de/update/{endpoint}?channel_guilded={message.channel.id}&"
                     f"webhook_guilded={webhook.url}&token={config.MASTER_TOKEN}")
-                await message.channel.send(f"Updated enpoint: http://api.guildcord.deutscher775.de/{endpoint}")
+                await message.channel.send(f"Updated enpoint: https://api.guildcord.deutscher775.de/{endpoint}")
         if message.content.startswith("gc!help"):
-            embed = guilded.Embed(title="Guildcord", description="API Docs: http://api.guildcord.deutscher775.de/docs")
+            embed = guilded.Embed(title="Guildcord", description="API Docs: https://api.guildcord.deutscher775.de/docs")
             embed.add_field(name="register",
                             value="Register you server.\nNote: First register your discord server, then guilded.",
+                            inline=False)
+            embed.add_field(name="set-log",
+                            value="Setup you log channel.\nUsage: DISCORD_SERVER_ID GUILDED_CHANNEL_ID",
+                            inline=False)
+            embed.add_field(name="allow",
+                            value="Allow a bot or webhook to be forwarded.\nUsage: DISCORD_SERVER_ID USER_ID",
                             inline=False)
             await message.channel.send(embed=embed)
         if message.content.startswith("gc!set-log"):
             try:
-                endpoint = int(message.content.replace("gc!register ", ""))
+                endpoint = int(message.content.replace("gc!set-log ", "").split(" ")[0])
             except ValueError:
-                await message.channel.send("Invalid Format: `gc!register DISCORD_SERVER_ID GUILDED_CHANNEL_ID`")
+                await message.channel.send("Invalid Format: `gc!set-log DISCORD_SERVER_ID GUILDED_CHANNEL_ID`")
                 return
             channelid = message.content.replace("gc!set-log ", "").split(" ")[1]
             if endpoint == "" or channelid == "":
-                await message.channel.send("Invalid Format: `gc!register DISCORD_SERVER_ID GUILDED_CHANNEL_ID`")
+                await message.channel.send("Invalid Format: `gc!set-log DISCORD_SERVER_ID GUILDED_CHANNEL_ID`")
             else:
                 requests.post(
-                    f"http://api.guildcord.deutscher775.de/update/{endpoint}?channel_guilded={message.channel.id}&"
+                    f"https://api.guildcord.deutscher775.de/update/{endpoint}?channel_guilded={message.channel.id}&"
                     f"log_guilded={channelid}&token={config.MASTER_TOKEN}")
-                await message.channel.send(f"Updated enpoint: http://api.guildcord.deutscher775.de/{endpoint}")
+                await message.channel.send(f"Updated enpoint: https://api.guildcord.deutscher775.de/{endpoint}")
+
+        if message.content.startswith("gc!allow"):
+            try:
+                endpoint = int(message.content.replace("gc!allow ", "").split(" ")[0])
+                print(endpoint)
+            except ValueError:
+                print(1)
+                await message.channel.send("Invalid Format: `gc!allow DISCORD_SERVER_ID GUILDED_USER_ID`")
+                return
+            allowid = message.content.replace("gc!allow ", "").split(" ")[1]
+            print(allowid)
+            if endpoint == "" or allowid == "":
+                await message.channel.send("Invalid Format: `gc!allow DISCORD_SERVER_ID GUILDED_USER_ID`")
+            else:
+                requests.post(
+                    f"https://api.guildcord.deutscher775.de/update/{endpoint}?allowed_ids={allowid}&token={config.MASTER_TOKEN}")
+                await message.channel.send(f"Updated enpoint: https://api.guildcord.deutscher775.de/{endpoint}")
     else:
         try:
             endpoint_file = open(f"{pathlib.Path(__file__).parent.resolve()}/guilded_servers/{message.server.id}.json",
                                  "r")
             endpoint = json.load(endpoint_file)["discord"]
             if message.channel.id == requests.get(
-                    f"http://api.guildcord.deutscher775.de/{endpoint}?token={config.MASTER_TOKEN}").json()[
+                    f"https://api.guildcord.deutscher775.de/{endpoint}?token={config.MASTER_TOKEN}").json()[
                 "config"]["channels"][
                 "guilded"]:
                 try:
                     blacklist = requests.get(
-                        f"http://api.guildcord.deutscher775.de/{endpoint}?token={config.MASTER_TOKEN}").json()[
+                        f"https://api.guildcord.deutscher775.de/{endpoint}?token={config.MASTER_TOKEN}").json()[
                         "config"]["blacklist"]
                     for word in blacklist:
                         if word is not None:
@@ -115,7 +151,7 @@ async def on_message(message: guilded.Message):
                                 embed = guilded.Embed(title=f"{message.author.name} - Flagged",
                                                       description=message.content, colour=0xf5c400)
                                 channel = await client.fetch_channel(str(requests.get(
-                                    f"http://api.guildcord.deutscher775.de/{endpoint}?token={config.MASTER_TOKEN}").json()[
+                                    f"https://api.guildcord.deutscher775.de/{endpoint}?token={config.MASTER_TOKEN}").json()[
                                                                              "config"]["logs"][
                                                                              "guilded"]))
                                 await channel.send(embed=embed)
@@ -123,14 +159,18 @@ async def on_message(message: guilded.Message):
                         else:
                             pass
                     else:
-                        if not message.author.bot:
+                        allowed_ids = requests.get(
+                            f"https://api.guildcord.deutscher775.de/{endpoint}?token={config.MASTER_TOKEN}").json()[
+                            "config"]["allowed-ids"]
+                        if not message.author.bot or message.author.id in allowed_ids:
                             if not message.attachments:
                                 requests.post(
-                                    f"http://api.guildcord.deutscher775.de/update/{endpoint}?message_content={message.content}&"
+                                    f"https://api.guildcord.deutscher775.de/update/{endpoint}?message_content={message.content}&"
                                     f"message_author_name={message.author.name}&message_author_avatar={message.author.avatar.url}&"
                                     f"message_author_id={message.author.id}&trigger=true&sender=guilded&token={config.MASTER_TOKEN}")
 
-                except requests.exceptions.JSONDecodeError:
+                except:
+                    traceback.print_exc()
                     print(2)
                     pass
         except FileNotFoundError:
