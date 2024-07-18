c81319fc38a8431bb27c008097aa62a47a5e95f3
Administrator <81298350+Deutscher775@users.noreply.github.com>
2024-07-18 16:03:20 +0200

    Update surrealdb_handler.py

--- src/astroidapi/surrealdb_handler.py
+++ src/astroidapi/surrealdb_handler.py
@@ -3,20 +3,13 @@ import os
 import json
 from Bot import config
 import astroidapi.errors as errors
-<<<<<<< Updated upstream
-
-async def sync_local_files(folderpath: str):
-=======
 import traceback
 
 async def sync_local_files(folderpath: str, specific: bool = False):
->>>>>>> Stashed changes
     try:
         async with Surreal(config.SDB_URL) as db:
             await db.signin({"user": config.SDB_USER, "pass": config.SDB_PASS})
             await db.use(config.SDB_NAMESPACE, config.SDB_DATABASE)
-<<<<<<< Updated upstream
-=======
             if specific:
                 with open(f"{folderpath}", "r") as file:
                     data = json.load(file)
@@ -28,23 +21,16 @@ async def sync_local_files(folderpath: str, specific: bool = False):
                         print("Creating")
                         await db.create(f'endpoints:`{file.name.replace(".json", "").split("/")[-1]}`', data)
                     return True
->>>>>>> Stashed changes
             for file in os.listdir(folderpath):
                 with open(f"{folderpath}/{file}", "r") as file:
                     data = json.load(file)
                     print(file.name)
                     try:
-<<<<<<< Updated upstream
-                        await db.query(f'CREATE endpoints:`{file.name.replace(".json", "").split("/")[-1]}` CONTENT {data}')
-                    except:
-                        await db.query(f'UPDATE endpoints:`{file.name.replace(".json", "").split("/")[-1]}` CONTENT {data}')
-=======
                         print("Updating")
                         await db.update(f'endpoints:`{file.name.replace(".json", "").split("/")[-1]}`', data)
                     except:
                         print("Creating")
                         await db.create(f'endpoints:`{file.name.replace(".json", "").split("/")[-1]}`', data)
->>>>>>> Stashed changes
             return True
     except Exception as e:
         raise errors.SurrealDBHandler.SyncLocalFilesError(e)
@@ -56,16 +42,6 @@ async def get_endpoint(endpoint: int):
             await db.signin({"user": config.SDB_USER, "pass": config.SDB_PASS})
             await db.use(config.SDB_NAMESPACE, config.SDB_DATABASE)
             data = await db.query(f"SELECT * FROM endpoints:`{endpoint}`")
-<<<<<<< Updated upstream
-            try:
-                data[0]["result"][0].pop("id") # Remove the id field from the data
-                return data[0]["result"][0]
-            except IndexError as e:
-                raise errors.SurrealDBHandler.GetEndpointError.EndpointNotFoundError(e)
-    except IndexError as e:
-        raise errors.SurrealDBHandler.GetEndpointError.EndpointNotFoundError(e)
-    except Exception as e:
-=======
             if not data:
                 raise errors.SurrealDBHandler.EndpointNotFoundError(f"Endpints exists, but data was found for endpoint {endpoint}")
             data[0]["result"][0].pop("id") # Remove the id field from the data
@@ -74,7 +50,6 @@ async def get_endpoint(endpoint: int):
         raise errors.SurrealDBHandler.EndpointNotFoundError(e)
     except Exception as e:
         traceback.print_exc()
->>>>>>> Stashed changes
         raise errors.SurrealDBHandler.GetEndpointError(f"[{endpoint}] {e}")
 
 async def create(endpoint: int, data: dict):
@@ -108,8 +83,6 @@ async def delete(endpoint: int):
         raise errors.SurrealDBHandler.DeleteEndpointError(e)
 
 
-<<<<<<< Updated upstream
-=======
 async def mark_read(endpoint, platform):
     try:
         print(f"Marking {platform} read")
@@ -123,4 +96,3 @@ async def mark_read(endpoint, platform):
     except Exception as e:
         raise errors.ReadHandlerError.MarkReadError(e)
 
->>>>>>> Stashed changes
